6594c0f8b64a8846b8f107e24873f71d28effde4
maivi <maximilianokadyszyn@gmail.com>
2017-04-20 13:05:55 -0300

    Prueba de host

--- include/conexion.php
+++ include/conexion.php
@@ -1,5 +1,5 @@
 <?php  
-	$host = "66.128.53.196"; 
+	$host = "66.128.53.196:2083"; 
 	$user = "mendozav_chappie";
 	$pw = "123qweasd.";
 	$db = "mendozav_nihuilcaribe";
--- include/logica.php
+++ include/logica.php
@@ -1,4 +1,10 @@
-<?php  
-	include "conexion.php";
-	
+<?php 
+ 
+include "conexion.php";
+
+$conexion = new mysqli($host,$user,$pw,$db);
+if ($mysqli->connect_errno) {
+	echo "Falló la conexión con MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+
 ?>
