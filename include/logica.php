0d49eb4e0cc1fe10f1c70c3af3ba1efb7c98a091
maivi <maximilianokadyszyn@gmail.com>
2017-04-20 16:29:12 -0300

    Terminadas las funciones de encapsulación de las sexo. Para que solo se ejecuten una vez

--- include/logica.php
+++ include/logica.php
@@ -16,13 +16,11 @@
 	}
 
 	$conexion->set_charset('utf8');
-
 	$conexion->query("SET NAMES 'UTF8'");
 
+	// Encapsulación de las Localidades
 	$consulta = "SELECT * FROM localidades;";
 	$result = $conexion->query($consulta);
-
-
 	$i=0;
 	while($actor = $result->fetch_assoc()){
 		echo $actor['id_localidades'] . " - ";
@@ -32,6 +30,21 @@
 		$json[$i]["localidades"] = $actor['localidades'];
 		$i++;
 	}
+	// Fin de la encapsulación
+
+	// Encapsulación de los Sexos
+	$consulta = "SELECT * FROM sexos;";
+	$result = $conexion->query($consulta);
+	$i=0;
+	while($actor = $result->fetch_assoc()){
+		echo $actor['id_sexo'] . " - ";
+		echo $actor['sexo'] . "<br>";
+
+		$json[$i]["id_sexo"] = $actor['id_sexo'];
+		$json[$i]["sexo"] = $actor['sexo'];
+		$i++;
+	}
+	// Fin de la encapsulación
 
 	echo json_encode($json); // Acá tenemos encapsuladas todas las localidades, para que solo se busquen una vez.
 
