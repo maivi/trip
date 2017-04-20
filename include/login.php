cb9287cf25d1f214192cffbb50f2e7ba75381cdb
Nathan747 <escorpion_erc@hotmail.com>
2017-04-20 13:45:14 -0300

    updates, se creo un inicio con un login, se grego el icono de nihuil

---
+++ include/login.php
@@ -0,0 +1,30 @@
+<div class="container">
+	<div class="row">
+		<div class="col-lg-4 col-lg-offset-4">
+			<div class="login">
+				<div class="usuario">
+					<div class="icono-login">
+						<img src="assets/images/user.png" alt="">
+					</div>
+					<div class="texto-login">
+						<input type="text" placeholder="USUARIO">
+					</div>	
+				</div>
+				<div class="password">
+					<div class="icono-login">
+						<img src="assets/images/key.png" alt="">
+					</div>
+					<div class="texto-login">
+						<input type="password" placeholder="CONTRASEÑA">
+					</div>	
+				</div>
+
+				<a href="#">
+					<div class="send">
+						INGRESAR
+					</div>
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
