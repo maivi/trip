78ce03f89160fca43486203a8ef4736a84ef2ea8
Nathan747 <escorpion_erc@hotmail.com>
2017-04-20 16:51:54 -0300

    Avances en el estilo de la home page y login

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
+	<link rel="shortcut icon" href="assets/images/favicon-01.png">
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="viewport" content="user-scalable=0, initial-scale=1.0">
