88edf2b9038acf0caf76116f1ccc94a7817f3c48
Nathan747 <escorpion_erc@hotmail.com>
2017-04-21 11:26:24 -0300

    se dieron estilos al login

--- index.php
+++ index.php
@@ -7,14 +7,20 @@
 	<meta name="viewport" content="user-scalable=0, initial-scale=1.0">
 	<title>DECI NIHUIL</title>
 	<?php 
-		include "include/styles.php";
+	include "include/styles.php";
 	?>
 </head>
 <body>
 	<?php 
-		include "include/login.php";
-		include "include/header.php";
-		include "include/scripts.php";
+	include "include/login.php";
+	include "include/header.php";
+	include "include/scripts.php";
+	include "include/form.php";
 	?>
+
+
+
+
+
 </body>
 </html>
