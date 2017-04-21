09665bf15377dec5ea406890b29e916aa1adf2d3
Nathan747 <escorpion_erc@hotmail.com>
2017-04-21 12:02:52 -0300

    se agrego register.php

---
+++ register.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<link rel="shortcut icon" href="assets/images/favicon-01.png">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="user-scalable=0, initial-scale=1.0">
+	<title>DECI NIHUIL</title>
+	<?php 
+	include "include/styles.php";
+	?>
+</head>
+<body>
+	<?php 
+	include "include/login.php";
+	include "include/header.php";
+	include "include/scripts.php";
+	include "include/form.php";
+	?>
+
+
+
+
+
+</body>
+</html>
