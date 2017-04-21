3e9a6c71fa221feae104fde8f729e7b3742d08e3
maivi <maximilianokadyszyn@gmail.com>
2017-04-21 15:01:37 -0300

    Cambios varios. No importantes

--- index.php
+++ index.php
@@ -1,22 +1,13 @@
 <!DOCTYPE html>
 <html lang="es">
-<head>
-	<link rel="shortcut icon" href="assets/images/favicon-01.png">
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="viewport" content="user-scalable=0, initial-scale=1.0">
-	<title>DECI NIHUIL</title>
-	<?php 
-	include "include/styles.php";
-	?>
-</head>
+<?php include "include/head.php"; ?>
 <body>
 	<?php 
 		session_start();
+		$_SESSION['newsession']='yes';
 		if ($_SESSION['newsession']=='yes'){
 			include "include/perfil.php";
 		}else{
-			echo "NO ENTRO";
 			header("Location: register.php");
 		}
 	?>
