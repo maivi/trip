e3ae96013922620fb8169acc82e1f37df9b203f5
maivi <maximilianokadyszyn@gmail.com>
2017-04-21 10:51:32 -0300

    Terminada la parte del Login con sus comprobaciones. Aun no testeada

--- include/logica_login.php
+++ include/logica_login.php
@@ -1,12 +1,31 @@
-<<<<<<< HEAD
+<?php  
+	$comprobar = $_POST["flag"];
+	$existe = 0;
 
+	switch ($comprobar){
+		case 0:{
+			$user = $_POST["user"];
+			$pw = $_POST["pw"];
+			$consulta = "SELECT * FROM usuarios WHERE user = '$user' AND pw = '$pw';";
+			$conexion->query($consulta);
+			$cantidad = $result->num_rows;
+			if($cantidad==1){
+				session_start();
+				$_SESSION['newsession']='yes';
+				while($usuario = $result->fetch_assoc()){
+					$_SESSION['usuario'] = $usuario['nombre'] . " " . $usuario['apellido'];
+				}
+				$existe=1;
+			}else{
+				$existe=0;
+			}
+			$json["existe"] = $existe;
+			echo json_encode($json);
+			$conexion->close();
 
+		}
+		case 1:{
 
-
-
-
-
-tester esteban push same time
-=======
-Prueba con el chappie
->>>>>>> 58991f3ca02652fe697fa6dbf68d4412c1659b09
+		}
+	}
+?>
