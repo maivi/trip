1e70c2f5818920b929019c66fd7a394c82d0e619
maivi <maximilianokadyszyn@gmail.com>
2017-04-24 10:10:53 -0300

    Corregidos los espacios

--- assets/js/new.js
+++ assets/js/new.js
@@ -1,81 +1,60 @@
 var nombre;
-
 var apellido;
-
 var email;
-
 var dni;
-
 var telefono;
-
 var localidad;
-
 var pw;
-
 var user;
-
 var sexo;
-
 var flag=0;
-
 
 $(document).ready(function(){
 	$(".submit1").click(function(e){
 
 		e.preventDefault();
-
 		console.log("CLICK");
-
 		$(".login").css("display","none");
-
 		$(".reg").css("display","block");
-
 		$(".login").animate({left: "-250%"},5000);
 	});
 
 
-$("#register").click(function(e){
-				nombre = $("#nombre").val();
-				apellido = $("#apellido").val();
-				email = $("#email").val();
-				dni = $("#dni").val();
-				telefono = $("#telefono").val();
-				localidad = $("#localidad").val();
-				pw = $("#pw").val();
-				user = $("#user").val();
-				sexo = $("#sexo").val();
-			e.preventDefault();
-			console.log("CLICK");
-			$.ajax({
-				url: "include/logica_login.php",
-				data: {
-					nombre: nombre, 
-					apellido: apellido,
-					email: email,
-					dni: dni,
-					telefono: telefono,
-					localidad: localidad,
-					pw:pw,
-					sexo:sexo,
-					user:user,
-					flag:1
-				},
+	$("#register").click(function(e){
+		nombre = $("#nombre").val();
+		apellido = $("#apellido").val();
+		email = $("#email").val();
+		dni = $("#dni").val();
+		telefono = $("#telefono").val();
+		localidad = $("#localidad").val();
+		pw = $("#pw").val();
+		user = $("#user").val();
+		sexo = $("#sexo").val();
+		e.preventDefault();
+		console.log("CLICK");
+		$.ajax({
+			url: "include/logica_login.php",
+			data: {
+				nombre: nombre, 
+				apellido: apellido,
+				email: email,
+				dni: dni,
+				telefono: telefono,
+				localidad: localidad,
+				pw:pw,
+				sexo:sexo,
+				user:user,
+				flag:1
+			},
+			method: "POST",
+			error: function(xhr, status, error) {
+				console.log(xhr);
+				console.log(status);
+				console.log(error);
+			}
 
-				method: "POST",
-
-				error: function(xhr, status, error) {
-
-					console.log(xhr);
-
-					console.log(status);
-
-					console.log(error);
-				}
-
-
-               }).done(function(json) {
-
-					console.log(json);
-});
-});
+		}).done(function(json) {
+			console.log(json);
+		});
+	});
 });
