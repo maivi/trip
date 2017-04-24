b996977ecbbcf6c018a31a941c3e9f3e97a735ec
maivi <maximilianokadyszyn@gmail.com>
2017-04-24 10:06:04 -0300

    Arreglados los espacios

--- include/form.php
+++ include/form.php
@@ -2,105 +2,91 @@
 	<div class="row">
 		<div class="col-lg-4 col-lg-offset-4">
 			<div class="reg">
+      
+        <form class="form-horizontal" role="form">
+          <div class="form-group">
+            <div class="col-lg-10">
+              <input type="text" class="form-control" id="nombre"
+              placeholder="Nombre">
+            </div>
+          </div>
 
-<form class="form-horizontal" role="form">
+          <div class="form-group">
+            <div class="col-lg-10">
+              <input type="text" class="form-control" id="apellido"
+              placeholder="Apellido">
+            </div>
+          </div>
 
-  <div class="form-group">
+          <div class="form-group">
+            <div class="col-lg-10">
+              <input type="email" class="form-control" id="email"
+              placeholder="Email">
+            </div>
+          </div>
 
-    <div class="col-lg-10">
-      <input type="text" class="form-control" id="nombre"
-             placeholder="Nombre">
+
+          <div class="form-group">
+            <div class="col-lg-10">
+              <input type="text" class="form-control" id="user"
+              placeholder="Usuario">
+            </div>
+          </div>
+
+
+          <div class="form-group">
+            <div class="col-lg-10">
+              <input type="password" class="form-control" id="pw" 
+              placeholder="Contraseña">
+            </div>
+          </div>
+
+
+          <div class="form-group">
+            <div class="col-lg-10">
+              <input type="number" class="form-control" id="dni" 
+              placeholder="DNI">
+            </div>
+          </div>
+
+          <div class="form-group">
+            <div class="col-lg-10">
+              <input type="number" class="form-control" id="telefono" 
+              placeholder="Teléfono">
+            </div>
+          </div>
+
+
+          <div class="form-group">
+            <div class="col-lg-10">
+              <select class="form-control" name="localidad" onchange="salta(this.form)" id="localidad">
+                <option selected> Gral Alvear
+                 <option value="yyyy.htm">Ciudad</option>
+                 <option value="zzzz.htm">Godoy Cruz</option>
+               </select>
+             </div>
+           </div>
+
+
+           <div class="form-group">
+            <div class="col-lg-10">
+              <select class="form-control" name="localidad" onchange="salta(this.form)" id="sexo">
+                <option selected> Hombre</option>
+                <option value="yyyy.htm">Mujer</option>
+              </select>
+            </div>
+          </div>
+
+          <div class="form-group">
+            <div class="col-lg-offset-2 col-lg-10">
+              <a  href=""><div class="btn btn-default">VOLVER</div></a>
+              <button  id="register" type="submit" class="btn btn-default">ENVIAR</button>
+            </div>
+          </div>
+
+
+        </form>
+      </div>
     </div>
   </div>
-
-    <div class="form-group">
-
-    <div class="col-lg-10">
-      <input type="text" class="form-control" id="apellido"
-             placeholder="Apellido">
-    </div>
-  </div>
-
-  <div class="form-group">
-
-    <div class="col-lg-10">
-      <input type="email" class="form-control" id="email"
-             placeholder="Email">
-    </div>
-  </div>
-
-
-    <div class="form-group">
-
-    <div class="col-lg-10">
-      <input type="text" class="form-control" id="user"
-             placeholder="Usuario">
-    </div>
-  </div>
-
-
-  <div class="form-group">
-
-    <div class="col-lg-10">
-      <input type="password" class="form-control" id="pw" 
-             placeholder="Contraseña">
-    </div>
-  </div>
- 
-
-  <div class="form-group">
-
-    <div class="col-lg-10">
-      <input type="number" class="form-control" id="dni" 
-             placeholder="DNI">
-    </div>
-  </div>
-
-  <div class="form-group">
-
-    <div class="col-lg-10">
-      <input type="number" class="form-control" id="telefono" 
-             placeholder="Teléfono">
-    </div>
-  </div>
-
-
-    <div class="form-group">
-
-    <div class="col-lg-10">
-    <select class="form-control" name="localidad" onchange="salta(this.form)" id="localidad">
-    <option selected> Gral Alvear
-         <option value="yyyy.htm">Ciudad
-         <option value="zzzz.htm">Godoy Cruz
-    </select>
-
-    </div>
-  </div>
-
-
-    <div class="form-group">
-
-    <div class="col-lg-10">
-    <select class="form-control" name="localidad" onchange="salta(this.form)" id="sexo">
-    <option selected> Hombre
-         <option value="yyyy.htm">Mujer
-    </select>
-
-    </div>
-  </div>
-
-  <div class="form-group">
-    <div class="col-lg-offset-2 col-lg-10">
-
-		<a  href=""><div class="btn btn-default">VOLVER</div></a>
-      <button  id="register" type="submit" class="btn btn-default">ENVIAR</button>
-    </div>
-  </div>
-
-
-</form>
-
-			</div>
-		</div>
-	</div>
 </div>
