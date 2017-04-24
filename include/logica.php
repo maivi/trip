779e6b0dfc21dc1b85f996487a87670101a02f1c
maivi <maximilianokadyszyn@gmail.com>
2017-04-24 16:42:45 -0300

    Agregada la busqueda de ID de la ultima pregunta

--- include/logica.php
+++ include/logica.php
@@ -53,6 +53,15 @@
 	}
 	// Fin de la encapsulación
 
+	// Ultimo ID pregunta
+	$consulta = "SELECT ultima_pregunta FROM ultima_pregunta;";
+	$result = $conexion->query($consulta);
+
+	if($actor = $result->fetch_assoc()){
+		$json["id_ultima_pregunta"] = $actor['ultima_pregunta'];
+	}
+	// Fin de la encapsulación del ID
+
 	echo json_encode($json); // Acá tenemos encapsuladas todas las localidades, para que solo se busquen una vez.
 
 	$conexion->close();
