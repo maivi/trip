fd1e037ab85b11224515867e03af64c0d828d362
Nathan747 <escorpion_erc@hotmail.com>
2017-04-25 16:41:22 -0300

    estilos de transicion

--- assets/js/new.js
+++ assets/js/new.js
@@ -84,14 +84,27 @@ $(document).ready(function(){
 		});
 	});
 
+
+
 	$(".submit1").click(function(e){
 		e.preventDefault();
 		console.log("CLICK");
-		$(".login").css("display","none");
-		$(".reg").css("display","block");
-		$(".login").animate({left: "-250%"},5000);
+		$(".reg").css("top","0px");
+
 
 	});
+
+		$(".equis").click(function(e){
+		e.preventDefault();
+		console.log("CLICK");
+		$(".reg").css("display","block");
+
+
+	});
+
+
+
+
 
 	$(".send").click(function(e){
 		e.preventDefault();
