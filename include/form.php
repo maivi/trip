fa442e82a1b344ba9c5ad65bb26373c8ad9c1a37
Nathan747 <escorpion_erc@hotmail.com>
2017-04-25 17:46:04 -0300

    edicion formulario, corregido ubicacion y arreglo container-fluid

--- include/form.php
+++ include/form.php
@@ -1,124 +1,131 @@
 
 <div class="reg">
-  <div class="box">
+ <div class="box">
+      <div class="container-fluid full-reg">
+           <div class="row">
+
+                       <div class="col-lg-12">
+                         <a class="cerrar" href="#">X</a>
+                       </div>
+
+                       <div class="col-lg-12">
+
+                        <h1 class="titulo1">REGISTRO</h1>
+
+                      </div>
+            </div>
+        </div>              
+
 <div class="container">
-	<div class="row">
+  <div class="row">
 
 
-<div class="col-lg-12">
+    <form class="form-horizontal" role="form">
 
-<h1 class="titulo1">REGISTRO</h1>   <a class="cerrar" href="#">X</a>
-
-</div>
-  <form class="form-horizontal" role="form">
-
-	<div class="col-lg-6">
-          <div class="form-group">
-            <div class="col-lg-12">
-              <input type="text" class="form-control" id="nombre"
-              placeholder="Nombre">
-            </div>
-          </div>
-  </div>
-  <div class="col-lg-6">
-
-          <div class="form-group">
-            <div class="col-lg-12">
-              <input type="text" class="form-control" id="apellido"
-              placeholder="Apellido">
-            </div>
-          </div>
-  </div>
-  <div class="col-lg-6">
-
-
-          <div class="form-group">
-            <div class="col-lg-12">
-              <input type="email" class="form-control" id="email"
-              placeholder="Email">
-            </div>
-          </div>
-
-  </div>
-  <div class="col-lg-6">        
-
-
-          <div class="form-group">
-            <div class="col-lg-12">
-              <input type="text" class="form-control" id="user"
-              placeholder="Usuario">
-            </div>
-          </div>
-
-  </div>
-  <div class="col-lg-6">
-
-          <div class="form-group">
-            <div class="col-lg-12">
-              <input type="password" class="form-control" id="pw" 
-              placeholder="Contraseña">
-            </div>
-          </div>
-
-</div>
-  <div class="col-lg-6">
-
-          <div class="form-group">
-            <div class="col-lg-12">
-              <input type="number" class="form-control" id="dni" 
-              placeholder="DNI">
-            </div>
-          </div>
-
-</div>
-  <div class="col-lg-6">
-
-
-          <div class="form-group">
-            <div class="col-lg-12">
-              <input type="number" class="form-control" id="telefono" 
-              placeholder="Teléfono">
-            </div>
-          </div>
-
-</div>
-  <div class="col-lg-6">
-
-          <div class="form-group">
-            <div class="col-lg-12">
-              <select class="form-control" name="localidad" id="localidad" placeholder="Ciudad">
-                
-               </select>
-             </div>
-           </div>
-
-</div>
-  <div class="col-lg-6">
-
-           <div class="form-group">
-            <div class="col-lg-12">
-              <select class="form-control" name="localidad" id="sexo">
-                
-              </select>
-            </div>
-          </div>
-
-
-</div>
-  <div class="col-lg-12">          
-
-          <div class="form-group">
-            <div class="col-lg-12">
-              <button  id="register" type="submit" class="btn btn-default">ENVIAR</button>
-            </div>
-          </div>
-</div>
-
-
-        
+     <div class="col-lg-6">
+      <div class="form-group">
+        <div class="col-lg-12">
+          <input type="text" class="form-control" id="nombre"
+          placeholder="Nombre">
+        </div>
       </div>
-      </form>
+    </div>
+
+
+    <div class="col-lg-6">
+      <div class="form-group">
+        <div class="col-lg-12">
+          <input type="text" class="form-control" id="apellido"
+          placeholder="Apellido">
+        </div>
+      </div>
+    </div>
+
+
+    <div class="col-lg-6">
+      <div class="form-group">
+        <div class="col-lg-12">
+          <input type="email" class="form-control" id="email"
+          placeholder="Email">
+        </div>
+      </div>
+    </div>
+
+
+
+    <div class="col-lg-6">        
+      <div class="form-group">
+        <div class="col-lg-12">
+          <input type="text" class="form-control" id="user"
+          placeholder="Usuario">
+        </div>
+      </div>
+    </div>
+
+
+    <div class="col-lg-6">
+      <div class="form-group">
+        <div class="col-lg-12">
+          <input type="password" class="form-control" id="pw" 
+          placeholder="Contraseña">
+        </div>
+      </div>
+    </div>
+
+
+    <div class="col-lg-6">
+      <div class="form-group">
+        <div class="col-lg-12">
+          <input type="number" class="form-control" id="dni" 
+          placeholder="DNI">
+        </div>
+      </div>
+    </div>
+
+
+
+    <div class="col-lg-6">
+      <div class="form-group">
+        <div class="col-lg-12">
+          <input type="number" class="form-control" id="telefono" 
+          placeholder="Teléfono">
+        </div>
+      </div>
+    </div>
+
+
+    <div class="col-lg-6">
+      <div class="form-group">
+        <div class="col-lg-12">
+          <select class="form-control" name="localidad" id="localidad" placeholder="Ciudad">
+          </select>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="col-lg-6">
+     <div class="form-group">
+      <div class="col-lg-12">
+        <select class="form-control" name="localidad" id="sexo">
+        </select>
+      </div>
     </div>
   </div>
-</div>
-</div>
+
+
+  <div class="col-lg-12">          
+    <div class="form-group">
+      <div class="col-lg-12">
+        <button  id="register" type="submit" class="btn btn-default">ENVIAR</button>
+      </div>
+    </div>
+  </div>
+
+</form>
+
+
+</div><!--fin row -->
+</div><!--fin container -->
+</div><!--fin box -->
+</div><!--fin reg -->
