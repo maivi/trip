367a840492f348982026048282cd41a8268ca822
maivi <maximilianokadyszyn@gmail.com>
2017-04-25 15:34:29 -0300

    Creacion del esqueleto del header

--- include/header.php
+++ include/header.php
@@ -1,18 +1,33 @@
-<header class="contenedor-header">
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-				<div class="icono-header">
-					<img src="assets/images/logo-nihuil.png" alt="">
-				</div>
-				<div class="bloque-derecho-header">
-					<div class="nombre-user"></div>
-					<div class="icono-user"></div>
-					<div class="cerrar-user">
-						
-					</div>
-				</div>	
-			</div>
+<nav class="navbar navbar-default">
+	<div class="container-fluid">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#">#DECÍNIHUIL</a>
 		</div>
-	</div>
-</header>
+
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			
+			<form class="navbar-form navbar-left">
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Email">
+				</div>
+				<div class="form-group">
+					<input type="password" class="form-control" placeholder="Contraseña">
+				</div>
+				<button type="submit" class="btn btn-default">ENTRAR</button>
+			</form>
+
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#">REGISTRARSE</a></li>
+				<li><a href="#">Perdí mi acceso</a></li>
+			</ul>
+		</div><!-- /.navbar-collapse -->
+	</div><!-- /.container-fluid -->
+</nav>
