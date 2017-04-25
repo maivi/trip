426758ceaa41e581de793730959f022ea9ae48fc
Nathan747 <escorpion_erc@hotmail.com>
2017-04-25 16:02:26 -0300

    formulario en dos columnas

--- include/form.php
+++ include/form.php
@@ -1,89 +1,114 @@
 <div class="container">
 	<div class="row">
-		<div class="col-lg-4 col-lg-offset-4">
-			<div class="reg">
+    <div class="reg">
+         <form class="form-horizontal" role="form">
 
-        <form class="form-horizontal" role="form">
+	<div class="col-lg-6">
           <div class="form-group">
-            <div class="col-lg-10">
+            <div class="col-lg-12">
               <input type="text" class="form-control" id="nombre"
               placeholder="Nombre">
             </div>
           </div>
+  </div>
+  <div class="col-lg-6">
 
           <div class="form-group">
-            <div class="col-lg-10">
+            <div class="col-lg-12">
               <input type="text" class="form-control" id="apellido"
               placeholder="Apellido">
             </div>
           </div>
+  </div>
+  <div class="col-lg-6">
+
 
           <div class="form-group">
-            <div class="col-lg-10">
+            <div class="col-lg-12">
               <input type="email" class="form-control" id="email"
               placeholder="Email">
             </div>
           </div>
 
+  </div>
+  <div class="col-lg-6">        
+
 
           <div class="form-group">
-            <div class="col-lg-10">
+            <div class="col-lg-12">
               <input type="text" class="form-control" id="user"
               placeholder="Usuario">
             </div>
           </div>
 
+  </div>
+  <div class="col-lg-6">
 
           <div class="form-group">
-            <div class="col-lg-10">
+            <div class="col-lg-12">
               <input type="password" class="form-control" id="pw" 
               placeholder="Contraseña">
             </div>
           </div>
 
+</div>
+  <div class="col-lg-6">
 
           <div class="form-group">
-            <div class="col-lg-10">
+            <div class="col-lg-12">
               <input type="number" class="form-control" id="dni" 
               placeholder="DNI">
             </div>
           </div>
 
+</div>
+  <div class="col-lg-6">
+
+
           <div class="form-group">
-            <div class="col-lg-10">
+            <div class="col-lg-12">
               <input type="number" class="form-control" id="telefono" 
               placeholder="Teléfono">
             </div>
           </div>
 
+</div>
+  <div class="col-lg-6">
 
           <div class="form-group">
-            <div class="col-lg-10">
+            <div class="col-lg-12">
               <select class="form-control" name="localidad" id="localidad" placeholder="Ciudad">
                 
                </select>
              </div>
            </div>
 
+</div>
+  <div class="col-lg-6">
 
            <div class="form-group">
-            <div class="col-lg-10">
+            <div class="col-lg-12">
               <select class="form-control" name="localidad" id="sexo">
                 
               </select>
             </div>
           </div>
 
+
+</div>
+  <div class="col-lg-12">          
+
           <div class="form-group">
-            <div class="col-lg-offset-2 col-lg-10">
-              <a  href=""><div class="btn btn-default">VOLVER</div></a>
+            <div class="col-lg-12">
               <button  id="register" type="submit" class="btn btn-default">ENVIAR</button>
             </div>
           </div>
+</div>
 
 
-        </form>
+        
       </div>
+      </form>
     </div>
   </div>
 </div>
