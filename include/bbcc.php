fc819c29f652611dc08e21f14f3fc2484928b8a6
Nathan747 <escorpion_erc@hotmail.com>
2017-04-26 10:55:43 -0300

    se agrego las bases y condiciones

---
+++ include/bbcc.php
@@ -0,0 +1,20 @@
+<div class="bbcc">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+
+			<<h1>PROMOCIÓN: “NIHUIL TE LLEVA AL CARIBE”</h1>
+				<h2>Bases y Condiciones</h2>
+
+			<p></p>	
+
+
+
+
+
+
+			</div>
+
+		</div>     
+	</div> 
+</div> 
