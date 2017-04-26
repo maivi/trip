c160512b725d50a3c178008a17c4e3aff044fd2e
maivi <maximilianokadyszyn@gmail.com>
2017-04-26 15:46:04 -0300

    Terminado el footer

--- include/footer.php
+++ include/footer.php
@@ -11,14 +11,26 @@
 				<div class="redes-contenedor">
 					<div class="redes">
 						<div class="red fb">
-							<i class="fa fa-facebook" aria-hidden="true"></i>
+							<a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+						</div>
+
+						<div class="red tw">
+							<a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+						</div>
+
+						<div class="red yt">
+							<a href="#"><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
+						</div>
+
+						<div class="red insta">
+							<a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
 						</div>
 					</div>
 				</div>
 
 				<div class="derechos-contenedor">
 					<div class="derechos-reservados">
-						
+						<p>2017 ©  Radio Nihuil. Todos los derechos reservados.     |     Desarrollado por <span><a href="https://koiron.com" target="_blank"><img src="assets/images/koiron.png" alt=""></a></span></p>
 					</div>
 				</div>
 			</div>
