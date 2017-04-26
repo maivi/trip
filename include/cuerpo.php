c5c01dfbc4057c5d4ad6c70530d49fb252b1ca8a
Nathan747 <escorpion_erc@hotmail.com>
2017-04-26 14:49:53 -0300

    se agrego el formulario multiple choice con el efecto del circulo

--- include/cuerpo.php
+++ include/cuerpo.php
@@ -26,8 +26,43 @@
 
 					echo $arrayDias[date('w')]." ".date('d')." de ".$arrayMeses[date('m')-1]." de ".date('Y');
 					?>
-					
-					
+
+					<div class="container question">
+
+						<div class="row">
+							<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+								
+
+								<form id="form" class="ac-custom ac-radio ac-circle" autocomplete="off">
+									<h2><span id="num">1°</span> PREGUNTA</h2>
+									<ul>
+										<li><input id="r1" name="r1" type="radio"><label for="r1">1 febrero 1994</label></li>
+										<li><input id="r2" name="r1" type="radio"><label for="r2">1 marzo 1994</label></li>
+										<li><input id="r3" name="r1" type="radio"><label for="r3">1 febrero 1993</label></li><br>
+									</ul>
+									<div id="contenedor-boton">
+										<button class="btn btn-default" type="submit">Enviar</button>
+									</div>
+
+								</form>
+
+
+
+
+							</div>
+						</div>
+					</div>
+
+					<script src="assets/js/jquery-1.12.min.js"></script>
+					<script src="assets/js/classie.js"></script>
+					<script src="assets/js/selectFx.js"></script>
+					<script src="assets/js/fullscreenForm.js"></script>
+					<script src="assets/js/svgcheckbx.js"></script>
+
+
+
+
+
 				</h4>
 			</div>
 		</div>
--- include/head.php
+++ include/head.php
@@ -3,6 +3,14 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="viewport" content="user-scalable=0, initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="assets/css/normalize.css" />
+		<link rel="stylesheet" type="text/css" href="assets/css/demo.css" />
+
+		<link rel="stylesheet" type="text/css" href="assets/css/component.css" />
+
+		<link rel="stylesheet" type="text/css" href="assets/css/cs-select.css" />
+		<link rel="stylesheet" type="text/css" href="assets/css/cs-skin-boxes.css" />
+		<script src="assets/js/modernizr.custom.js"></script>
 	<title>DECI NIHUIL</title>
 	<?php 
 	include "include/styles.php";
