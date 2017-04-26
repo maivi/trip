9ea3504012e7c0590b4532e44ad4bfc1eb9c7c7a
Nathan747 <escorpion_erc@hotmail.com>
2017-04-26 12:40:20 -0300

    modificacion fecha

--- include/cuerpo.php
+++ include/cuerpo.php
@@ -16,15 +16,17 @@
 		<div class="row">
 			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 pregunta">
 				<h4 class="consigna">Consigna #1 - 
-				<?php
-				$arrayMeses = array('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
-					'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre');
+					<?php
 
-				$arrayDias = array( 'Domingo', 'Lunes', 'Martes',
-					'Miercoles', 'Jueves', 'Viernes', 'Sabado');
+					$arrayMeses = array('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
+						'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre');
 
-				echo $arrayDias[date('w')]." ".date('d')." de ".$arrayMeses[date('m')-1]." de ".date('Y');
-				?>
+					$arrayDias = array( 'Domingo', 'Lunes', 'Martes',
+						'Miercoles', 'Jueves', 'Viernes', 'Sabado');
+
+					echo $arrayDias[date('w')]." ".date('d')." de ".$arrayMeses[date('m')-1]." de ".date('Y');
+					?>
+					
 				</h4>
 			</div>
 		</div>
