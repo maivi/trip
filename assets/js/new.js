fb253497ccb9b535725e22a5e633e9de000a8694
maivi <maximilianokadyszyn@gmail.com>
2017-04-27 02:44:06 -0300

    Muchas optimizaciones en el tiempo de espera y carga de la página pero aun se puede mejorar

--- assets/js/new.js
+++ assets/js/new.js
@@ -16,139 +16,137 @@ var rellenar = $("#form").find("ul");
 $(document).ready(function(){
 
 	//Comprobamos que no exista los localstorage
-	
+	if ( ( (typeof localStorage["respondida"])==="undefined") ){
+		localStorage["respondida"]= "No";
+	}
+
+	if ((localStorage["respondida"] == "Si") && (localStorage["logged"] == "Si")) {
+		$("#form").find("ul").empty();
+		$("#form").find("h2").empty();
+		$("#form").find("h2").append("Gracias por participar. Volvé mañana por una nueva pregunta");
+		$("#contenedor-boton").empty();
+	}
+
+	if ( ( (typeof localStorage["pregunta_1"])!="undefined") && ( (typeof localStorage["pregunta_2"])!="undefined" ) && ( (typeof localStorage["pregunta_3"])!="undefined" ) && (localStorage["ultimo_dia"] == dia) ){
+		if ((localStorage["respondida"]= "Si") && (localStorage["logged"] == "Si") ){
+			$("#form").find("ul").empty();
+			$("#form").find("h2").empty();
+			$("#form").find("h2").append("Gracias por participar. Volvé mañana por una nueva pregunta");
+			$("#contenedor-boton").empty();
+		}
+
+		// Si es el mismo día, directamente trae las respuestas cacheadas
+		rellenar.find("li:nth-child(1)").find("label").empty();
+		rellenar.find("li:nth-child(1)").find("label").append(localStorage["resp_1"]);
+		rellenar.find("li:nth-child(2)").find("label").empty();
+		rellenar.find("li:nth-child(2)").find("label").append(localStorage["resp_2"]);
+		rellenar.find("li:nth-child(3)").find("label").empty();
+		rellenar.find("li:nth-child(3)").find("label").append(localStorage["resp_3"]);
+
+
+	}
+
 	$.ajax({
 		url: "include/logica.php",
 		method: "POST"
 	})
 	.done(function(json) {
-			//Cache de las localidades y los sexos
-			var objeto = $.parseJSON(json);
-			var i=0;
-			var localidades = $("#localidad");
-			var sexos = $("#sexo");
-			var constructor;
-			var cant_localidades = 0;
-			var cant_sexos = 0;
-			console.log(objeto.length);
-			console.log(objeto);
+		//Cache de las localidades y los sexos
+		var objeto = $.parseJSON(json);
+		var constructor;
+		if ( (localStorage["respondida"]=="Si") && (localStorage["logged"] == "Si") ){
+			$("#form").find("ul").empty();
+			$("#form").find("h2").empty();
+			$("#form").find("h2").append("Gracias por participar. Volvé mañana por una nueva pregunta");
+			$("#contenedor-boton").empty();
+		}
 
-			while(i<objeto.length){
-				if("undefined" != typeof objeto[i]["localidades"]){
-					cant_localidades++;
-				}
-
-				if("undefined" != typeof objeto[i]["sexo"]){
-					cant_sexos++;
-				}
-				i++;
-			}
-
-			i=0;
-			while(i<cant_localidades){
-				constructor = "<option value='"+(i+1)+"'>"+objeto[i]["localidades"]+"</option>";
-				localidades.append(constructor);
-				i++;
-			}
-
-			i=0;
-			while(i<cant_sexos){
-				constructor = "<option value='"+(i+1)+"'>"+objeto[i]["sexo"]+"</option>";
-				sexos.append(constructor);
-				i++;
-			}
-			i=0;
-
-			if ( ( (typeof localStorage["ultimo_id"])==="undefined") && ( (typeof localStorage["ultimo_dia"])==="undefined" ) ){
+		if ( ( (typeof localStorage["ultimo_id"])==="undefined") && ( (typeof localStorage["ultimo_dia"])==="undefined" ) ){
 			// Creamos los 2 localstorage para Dia e ID
 			localStorage["ultimo_id"] = objeto[0]["id_ultima_pregunta"];
 			localStorage["ultimo_dia"] = dia;
 		}
-			//Checkeamos si estan creados los localstorages de las preguntas para cachearlos
-			if ( ( (typeof localStorage["pregunta_1"])==="undefined") && ( (typeof localStorage["pregunta_2"])==="undefined" ) && ( (typeof localStorage["pregunta_3"])==="undefined" ) ){
 
-				$.ajax({
-					url: "include/logica_respuestas.php",
-					data: {
-						id:localStorage["ultimo_id"]
-					},
-					method: "POST"
-				})
-				.done(function(test){
-					console.log(test);
-					var objeto = $.parseJSON(test);
-					localStorage["resp_1"] = objeto[0]["resp_1"];
-					localStorage["resp_2"] = objeto[0]["resp_2"];
-					localStorage["resp_3"] = objeto[0]["resp_3"];
+		//Checkeamos si estan creados los localstorages de las preguntas para cachearlos
+		if ( ( (typeof localStorage["pregunta_1"])==="undefined") && ( (typeof localStorage["pregunta_2"])==="undefined" ) && ( (typeof localStorage["pregunta_3"])==="undefined" ) ){
 
-					// Cacheamos las respuestas para no tener que volver a realizar las consultas
+			$.ajax({
+				url: "include/logica_respuestas.php",
+				data: {
+					id:localStorage["ultimo_id"]
+				},
+				method: "POST"
+			})
+			.done(function(test){
+				console.log(test);
+				var objeto = $.parseJSON(test);
+				localStorage["resp_1"] = objeto[0]["resp_1"];
+				localStorage["resp_2"] = objeto[0]["resp_2"];
+				localStorage["resp_3"] = objeto[0]["resp_3"];
 
-					rellenar.find("li:nth-child(1)").find("label").empty();
-					rellenar.find("li:nth-child(1)").find("label").append(localStorage["resp_1"]);
-					rellenar.find("li:nth-child(2)").find("label").empty();
-					rellenar.find("li:nth-child(2)").find("label").append(localStorage["resp_2"]);
-					rellenar.find("li:nth-child(3)").find("label").empty();
-					rellenar.find("li:nth-child(3)").find("label").append(localStorage["resp_3"]);
+				// Cacheamos las respuestas para no tener que volver a realizar las consultas
+
+				rellenar.find("li:nth-child(1)").find("label").empty();
+				rellenar.find("li:nth-child(1)").find("label").append(localStorage["resp_1"]);
+				rellenar.find("li:nth-child(2)").find("label").empty();
+				rellenar.find("li:nth-child(2)").find("label").append(localStorage["resp_2"]);
+				rellenar.find("li:nth-child(3)").find("label").empty();
+				rellenar.find("li:nth-child(3)").find("label").append(localStorage["resp_3"]);
 
 					//Aca cambiamos las preguntas con las ya cacheadas
 				})
-				.fail(function(xhr, status, error){
-					console.log(xhr);
-					console.log(status);
-					console.log(error);
-					console.log("FAIL");
+			.fail(function(xhr, status, error){
+				console.log(xhr);
+				console.log(status);
+				console.log(error);
+				console.log("FAIL");
+			});
+		}else{
+			// Si existe el localstorage, comprobamos el dia para realizar el cambio de id de pregunta
+			if(localStorage["ultimo_dia"] != dia){
+				$.ajax({
+					url: "include/ultimo_dia.php",
+					method: "POST"
+				}).done(function(json) {
+					var objeto = $.parseJSON(json);
+					localStorage["ultimo_id"] = objeto[0]["id_ultima_pregunta"];
+					localStorage["ultimo_dia"] = dia;
+					//Nuevamente, cacheamos las respuestas de ese dia
+
+					$.ajax({
+						url: "include/logica_respuestas.php",
+						data: {
+							id:localStorage["ultimo_id"]
+						},
+						method: "POST"
+					})
+					.done(function(json){
+						console.log(json);
+						var objeto = $.parseJSON(json);
+						localStorage["resp_1"] = objeto[0]["resp_1"];
+						localStorage["resp_2"] = objeto[0]["resp_2"];
+						localStorage["resp_3"] = objeto[0]["resp_3"];
+						rellenar.find("li:nth-child(1)").find("label").empty();
+						rellenar.find("li:nth-child(1)").find("label").append(localStorage["resp_1"]);
+						rellenar.find("li:nth-child(2)").find("label").empty();
+						rellenar.find("li:nth-child(2)").find("label").append(localStorage["resp_2"]);
+						rellenar.find("li:nth-child(3)").find("label").empty();
+						rellenar.find("li:nth-child(3)").find("label").append(localStorage["resp_3"]);
+					})
+					.fail(function(xhr, status, error){
+						console.log(xhr);
+						console.log(status);
+						console.log(error);
+						console.log("FAIL");
+					});
+					localStorage["respondida"]="No";
 				});
 			}else{
-				// Si existe el localstorage, comprobamos el dia para realizar el cambio de id de pregunta
-				if(localStorage["ultimo_dia"] != dia){
-					$.ajax({
-						url: "include/ultimo_dia.php",
-						method: "POST"
-					}).done(function(json) {
-						var objeto = $.parseJSON(json);
-						localStorage["ultimo_id"] = objeto[0]["id_ultima_pregunta"];
-						localStorage["ultimo_dia"] = dia;
-						//Nuevamente, cacheamos las respuestas de ese dia
 
-						$.ajax({
-							url: "include/logica_respuestas.php",
-							data: {
-								id:localStorage["ultimo_id"]
-							},
-							method: "POST"
-						})
-						.done(function(json){
-							console.log(json);
-							var objeto = $.parseJSON(json);
-							localStorage["resp_1"] = objeto[0]["resp_1"];
-							localStorage["resp_2"] = objeto[0]["resp_2"];
-							localStorage["resp_3"] = objeto[0]["resp_3"];
-							rellenar.find("li:nth-child(1)").find("label").empty();
-							rellenar.find("li:nth-child(1)").find("label").append(localStorage["resp_1"]);
-							rellenar.find("li:nth-child(2)").find("label").empty();
-							rellenar.find("li:nth-child(2)").find("label").append(localStorage["resp_2"]);
-							rellenar.find("li:nth-child(3)").find("label").empty();
-							rellenar.find("li:nth-child(3)").find("label").append(localStorage["resp_3"]);
-						})
-						.fail(function(xhr, status, error){
-							console.log(xhr);
-							console.log(status);
-							console.log(error);
-							console.log("FAIL");
-						});
-					});
-				}else{
-					// Si es el mismo día, directamente trae las respuestas cacheadas
-					rellenar.find("li:nth-child(1)").find("label").empty();
-					rellenar.find("li:nth-child(1)").find("label").append(localStorage["resp_1"]);
-					rellenar.find("li:nth-child(2)").find("label").empty();
-					rellenar.find("li:nth-child(2)").find("label").append(localStorage["resp_2"]);
-					rellenar.find("li:nth-child(3)").find("label").empty();
-					rellenar.find("li:nth-child(3)").find("label").append(localStorage["resp_3"]);
 
-				}
 			}
-		})
+		}
+	})
 .fail(function(xhr, status, error){
 	console.log(xhr);
 	console.log(status);
@@ -173,6 +171,24 @@ $(document).ready(function(){
 			reemplazar = reemplazar.replace("r","");
 			reemplazar = parseInt(reemplazar);
 			console.log(reemplazar);
+		});
+
+		$.ajax({
+			url: "include/logica_login.php",
+			data: {
+				respuesta: reemplazar,
+				id_pregunta: localStorage["ultimo_id"]
+			},
+			method: "POST"
+
+		}).done(function(json) {
+			localStorage["respondida"] = "Si";
+			$("#form").find("ul").empty();
+			$("#form").find("h2").empty();
+			$("#form").find("h2").append("Gracias por participar. Volvé mañana por una nueva pregunta");
+		})
+		.fail(function(xhr, status, error){
+
 		});
 	});
 
@@ -240,6 +256,7 @@ $(document).ready(function(){
 					eliminar.empty();
 					eliminar.append("Usuario o Password incorrecto");	
 				}else{
+					localStorage["logged"] = "Si";
 					window.location="index.php";
 				}
 				console.log(obj["existe"]);
