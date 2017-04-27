ea8940fd98d7429d24cf16a7ab53653d65a72b15
maivi <maximilianokadyszyn@gmail.com>
2017-04-27 02:42:44 -0300

    Eliminado lo que no servia para acelerar la carga

--- include/logica.php
+++ include/logica.php
@@ -11,39 +11,6 @@
 	$conexion->set_charset('utf8');
 	$conexion->query("SET NAMES 'UTF8'");
 
-	// Encapsulación de las Localidades
-	$consulta = "SELECT * FROM localidades;";
-	$result = $conexion->query($consulta);
-	$i=0;
-	while($actor = $result->fetch_assoc()){
-		$json[$i]["id_localidades"] = $actor['id_localidades'];
-		$json[$i]["localidades"] = $actor['localidades'];
-		$i++;
-	}
-	// Fin de la encapsulación
-
-	// Encapsulación de los Sexos
-	$consulta = "SELECT * FROM sexos;";
-	$result = $conexion->query($consulta);
-	$i=0;
-	while($actor = $result->fetch_assoc()){
-		$json[$i]["id_sexo"] = $actor['id_sexo'];
-		$json[$i]["sexo"] = $actor['sexo'];
-		$i++;
-	}
-	// Fin de la encapsulación
-
-	// Encapsulación de los Estados
-	$consulta = "SELECT * FROM estado_respuesta;";
-	$result = $conexion->query($consulta);
-	$i=0;
-	while($actor = $result->fetch_assoc()){
-		$json[$i]["id_estado"] = $actor['id_estado'];
-		$json[$i]["estado_respuesta"] = $actor['estado_respuesta'];
-		$i++;
-	}
-	// Fin de la encapsulación
-
 	// Ultimo ID pregunta
 	$consulta = "SELECT ultima_pregunta FROM ultima_pregunta;";
 	$result = $conexion->query($consulta);
