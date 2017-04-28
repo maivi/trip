53c2fd01dfd4f88127f117750a1ce0c50a0344df
Nathan747 <escorpion_erc@hotmail.com>
2017-04-28 17:01:27 -0300

    cambio de logo del header de nihuil

--- include/header.php
+++ include/header.php
@@ -8,7 +8,9 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="http://radionihuil.com.ar/">#DECÍNIHUIL</a>
+			<a class="navbar-brand" href="http://radionihuil.com.ar/">
+				<img src="assets/images/logo.png">
+			</a>
 		</div>
 
 		<!-- Collect the nav links, forms, and other content for toggling -->
